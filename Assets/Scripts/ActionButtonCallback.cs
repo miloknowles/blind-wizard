1b4ce07525dda2a515bc97c96c9a62bd48549b02
Milo Knowles <mknowles@mit.edu>
2019-04-23 13:43:37 -0400

    Clicking on attacks

--- Assets/Scripts/ActionButtonCallback.cs
+++ Assets/Scripts/ActionButtonCallback.cs
@@ -18,6 +18,8 @@ public class ActionButtonCallback : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("Setting up button with attack: " + thisButtonAttack);
+
         // Set up click event handler.
         this.gameObject.GetComponent<Button>().onClick.AddListener(() => this.OnClick());
         
