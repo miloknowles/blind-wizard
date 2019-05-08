7731a3399d96ea8d581e4ab26b4a090d68adfe0d
Milo Knowles <mknowles@mit.edu>
2019-05-08 15:05:37 -0400

    Reveals enemy type on win (#22)

---
+++ Assets/Scripts/DisplayEnemyTypeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayEnemyTypeScript : MonoBehaviour
+{
+    public TMP_Text infoText;
+    public GameObject revealObjects;
+    public GameObject upgradeObjects;
+    private string preposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        preposition = " a ";
+        upgradeObjects.SetActive(false);
+        revealObjects.SetActive(true);
+        if (GameStateManager.UpcomingEnemyStats.Element.ToString() == "Earth" || GameStateManager.UpcomingEnemyStats.Element.ToString() == "Air")
+        {
+            preposition = " an ";
+        }
+        infoText.text = "Congratulations, you defeated "+ preposition + GameStateManager.UpcomingEnemyStats.Element.ToString() + " type enemy!";
+    }
+
+    public void SwitchToUpgrade()
+    {
+        revealObjects.SetActive(false);
+        upgradeObjects.SetActive(true);
+    }
+
+}
