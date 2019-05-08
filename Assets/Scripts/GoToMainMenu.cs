29dac16258e74c5975bb942de822b0ddac0dff5c
casparmkerr <casparmkerr@gmail.com>
2019-05-08 15:17:53 -0400

    Reveals enemy on game over

--- Assets/Scripts/GoToMainMenu.cs
+++ Assets/Scripts/GoToMainMenu.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 /*
  * Attached to the gameover / win scenes, handles going back to the main menu when a game is finished.
  */
 public class GoToMainMenu : MonoBehaviour
 {
+    public TMP_Text infoText;
+    private string preposition;
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(() => this.OnClick());
+        preposition = " a ";
+        if (GameStateManager.UpcomingEnemyStats.Element.ToString() == "Earth" || GameStateManager.UpcomingEnemyStats.Element.ToString() == "Air")
+        {
+            preposition = " an ";
+        }
+        infoText.text = "Oh no! You were defeated by" + preposition + GameStateManager.UpcomingEnemyStats.Element.ToString() + " type enemy! Try paying more attention to the provided information next time, to more effectively identify and defeat your enemies.";
+
     }
     private void OnClick()
     {
