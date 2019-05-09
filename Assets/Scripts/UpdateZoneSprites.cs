d62bdb9102088030677f3381c79a1d55e15af404
Charlie <ctonn@mit.edu>
2019-05-08 21:39:30 -0400

    Fix text issue

--- Assets/Scripts/UpdateZoneSprites.cs
+++ Assets/Scripts/UpdateZoneSprites.cs
@@ -5,9 +5,23 @@ using Primitives;
 
 public class UpdateZoneSprites : MonoBehaviour
 {
-    private GameObject[] zones;      void Start()     {         zones = GameObject.FindGameObjectsWithTag("BattleTriggerNode");          foreach (GameObject zone in zones)
-        {             Region random_region = ProbabilitySystem.SampleRegionUniform();             string spr = "";              if (random_region == Region.City)             {                 spr = "city";             }             else if (random_region == Region.Forest)             {                 spr = "forest";             }             else if (random_region == Region.Mountain)             {                 spr = "mountain";             }             else if (random_region == Region.Storm)             {                 spr = "storm";             }             else if (random_region == Region.Plains)             {                 spr = "plains";             }             else if (random_region == Region.Village)             {                 spr = "village";             }              zone.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(spr);
-
+    private GameObject[] zones;
+     void Start()     {
+        zones = GameObject.FindGameObjectsWithTag("BattleTriggerNode");         foreach (GameObject zone in zones)
+        {
+            Region random_region = ProbabilitySystem.SampleRegionUniform();             string spr = "";
+             if (random_region == Region.City)             {
+                spr = "city";             }
+            else if (random_region == Region.Forest)             {
+                spr = "forest";
+            }
+            else if (random_region == Region.Mountain)             {                 spr = "mountain";             }
+            else if (random_region == Region.Storm)             {
+                spr = "storm";             }
+            else if (random_region == Region.Plains)             {
+                spr = "plains";             }
+            else if (random_region == Region.Village)             {
+                spr = "village";             }             zone.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(spr);
         }
     }
 } 
