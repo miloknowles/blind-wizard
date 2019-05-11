6fbbb5be8a21390a0d82bad60ec494184f2b6217
Milo Knowles <mknowles@mit.edu>
2019-05-11 15:28:41 -0400

    Toggle behaviour

---
+++ Assets/Scripts/TutorialModeToggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialModeToggle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Toggle>().onValueChanged.AddListener((isSelected) => {
+            GameStateManager.MapState.TutorialMode = isSelected;
+        });
+    }
+}
